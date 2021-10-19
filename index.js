9bd093332a02a1ef939cbbf00f6ec39fb5b9f39d
andrewroddy92 <andrewroddy92@yahoo.com>
2021-10-19 19:55:04 -0400

    Added question loop for adding team members

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ var manager;
 var engineers = []; 
 var interns = [];
 
+const employeeSelection = [{
+    type: 'list',
+    name: 'employeeType',
+    message: 'Which type of employee would you like to add?',
+    choices: ['Manager', 'Engineer', 'Intern', 'None'],
+}]
+
+
 const managerQuestions = [{
         type: 'input',
         name: 'managerName',
@@ -30,22 +38,90 @@ const managerQuestions = [{
     },
 ]
 
-function addTeamMembers() {
-    //TODO: Add code for new questions, which loop
-    console.log("Ready to ask team member questions!");
+const engineerQuestions = [{
+    type: 'input',
+    name: 'engineerName',
+    message: 'Engineer Name:',
+},
+{
+    type: 'input',
+    name: 'engineerId',
+    message: 'Engineer Employee ID:',
+},
+{
+    type: 'input',
+    name: 'engineerEmail',
+    message: 'Engineer Email:',
+},
+{
+    type: 'input',
+    name: 'engineerGithub',
+    message: 'Engineer Github Username:',
+},
+]
+
+const internQuestions = [{
+    type: 'input',
+    name: 'internName',
+    message: 'Intern Name:',
+},
+{
+    type: 'input',
+    name: 'internId',
+    message: 'Intern Employee ID:',
+},
+{
+    type: 'input',
+    name: 'internEmail',
+    message: 'Intern Email:',
+},
+{
+    type: 'input',
+    name: 'internSchool',
+    message: 'Intern School:',
+},
+]
+
+function addManager() {
+    inquirer.prompt(managerQuestions).then((managerAnswers) => {
+        manager = new Manager(managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.managerOfficeNumber)
+        init()
+    });
+}
+
+function addEngineer() {
+    inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+        var engineer = new Manager(engineerAnswers.engineerName, engineerAnswers.engineerId, engineerAnswers.engineerEmail, engineerAnswers.engineerGithub)
+        engineers.push(engineer);
+        init()
+    });
+}
+
+function addIntern() {
+    inquirer.prompt(internQuestions).then((internAnswers) => {
+        var intern = new Intern(internAnswers.internName, internAnswers.internId, internAnswers.internEmail, internAnswers.internOfficeNumber)
+        interns.push(intern);
+        init()
+    });
 }
 
 function init() {
-    inquirer.prompt(managerQuestions).then((answers) => {
-        var inputManager = new Manager(
-            answers.managerOfficeNumber,
-            answers.managerName,
-            answers.managerId,
-            answers.managerEmail
-            )
-        console.log(inputManager);
-        manager = inputManager;
-        addTeamMembers();
+    inquirer.prompt(employeeSelection).then((answers) => {
+        switch (answers.employeeType){
+            case 'Manager':
+                addManager()
+                break       
+            case 'Engineer': 
+                addEngineer()
+                break
+            case 'Intern':
+                addIntern()
+                break
+            case 'None': 
+                console.log('tallboy')
+                break
+                 
+        }
     });
 }
 
